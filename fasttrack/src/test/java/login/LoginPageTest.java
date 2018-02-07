bd6d2348a6b8b2de67a4ae25c09a557506ba3dbf
sudheer <sudheer@192.168.0.2>
2018-02-07 10:21:29 +0530

    this is new project

---
+++ fasttrack/src/test/java/login/LoginPageTest.java
@@ -0,0 +1,16 @@
+package login;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
+
+public class LoginPageTest {
+	WebDriver driver;
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
---
+++ fasttrack/src/test/java/login/SignupTest.java
@@ -0,0 +1,14 @@
+package login;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class SignupTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
